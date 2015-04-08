b17229b784290d8c59337a0bc50e2b9568f9ead4
Ryan Nagle <rmnagle@gmail.com>
2015-04-08 17:16:30 -0500

    add a prompt for the mysql root user password

--- dump_tables.sh
+++ dump_tables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 args=("$@")
 if [ $# -gt 0 ]; then
-    mysqldump --user=root --password ${args[0]} --no-create-info wp_users wp_usermeta > network_tables.sql
-    mysqldump --user=root --password ${args[0]} --ignore-table=${args[0]}.wp_users --ignore-table=${args[0]}.wp_usermeta > blog_tables.sql
+    echo -n "Enter the password for you MySQL 'root' user: "
+    read -s password
+    mysqldump --user=root --password=$password ${args[0]} --no-create-info wp_users wp_usermeta > ${args[0]}_network_tables.sql
+    mysqldump --user=root --password=$password ${args[0]} --ignore-table=${args[0]}.wp_users --ignore-table=${args[0]}.wp_usermeta > ${args[0]}_blog_tables.sql
 else
     echo "This script dumps a WordPress database stored locally, split into network and blog"
     echo "tables. It uses the root MySQL user and will prompt you for the password."
