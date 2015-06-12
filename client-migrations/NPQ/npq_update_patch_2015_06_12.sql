3596cadb59967dbab45f6cb2f623a6aa42a4e9fd
Ryan Nagle <rmnagle@gmail.com>
2015-06-12 16:38:44 -0500

    a new migration file for npq's june update before launch

---
+++ client-migrations/NPQ/npq_update_patch_2015_06_12.sql
@@ -0,0 +1,166 @@
+-- REMEMBER: After loading the prepared tables into vagrant, staging or production --
+-- Check siteurl, home, upload_url_path in wp_NEWBLOGID_options table.
+-- Change wp_user_roles to wp_NEWBLOGID_user_roles in wp_NEWBLOGID_options table.
+
+-- Prefixes are needed to push the new users/usermeta values outside of the range already populated
+-- in the database, usually for a site with an ID of 40 using 40,000 will work pretty well (unless
+-- some of your sites have a lot of users).
+
+-- Set the `@newUserID` and `@newUmetaID` to something sufficiently high.
+-- Try running:
+-- SELECT max(umeta_id) FROM wp_usermeta;
+-- and
+-- SELECT max(ID) FROM wp_users;
+-- to get an idea of where to start.
+SET @newUmetaID = 5760723;
+SET @newUserID = 2563112;
+SET @newBlogID = 56;
+
+-- NOTE: SET POST ID
+-- Set all ID's to an appropriate offset
+-- select max(ID) from wp_56_posts;
+SET @newPostID = 34797 + 100;
+UPDATE wp_posts SET ID = ID + @newPostID;
+
+-- NOTE: SET POST META ID
+-- select max(meta_id) from wp_56_postmeta;
+-- SET @newPostMetaID = 41277 + 100;
+-- UPDATE wp_postmeta SET meta_id = meta_id + @newPostMetaID;
+
+-- NOTE: SET COMMENT ID
+-- select max(comment_ID) from wp_56_comments;
+SET @newCommentID = 12413 + 100;
+UPDATE wp_comments SET comment_ID = comment_ID + @newCommentID;
+
+-- NOTE: SET COMMENT META ID
+-- select max(meta_id) from wp_56_commentmeta;
+-- SET @newCommentMetaID = 0 + 100;
+-- UPDATE wp_commentmeta SET meta_id = meta_id + @newCommentMetaID;
+
+
+-- We don't need to mess with the term tables apparently
+
+-- NOTE: SET TERM REL ID
+-- select max(object_id) from wp_56_term_relationships;
+-- SET @newTermRelObjID = 34373 + 100;
+-- UPDATE wp_term_relationships SET object_id = object_id + @newTermRelObjID;
+
+-- NOTE: SET TERM TAX ID
+-- select max(term_taxonomy_id) from wp_56_term_taxonomy;
+-- SET @newTermTaxID = 14920 + 100;
+-- UPDATE wp_term_taxonomy SET term_taxonomy_id = term_taxonomy_id + @newTermTaxID;
+
+-- NOTE: SET TERM ID
+-- select max(term_id) from wp_56_terms;
+-- SET @newTermID = 48143 + 100;
+-- UPDATE wp_terms SET term_id = term_id + @newTermID;
+
+
+-- DON'T CHANGE STUFF BELOW THIS LINE ---
+
+-- Rename tables
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_commentmeta TO wp_', @newBlogID, '_commentmeta;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_comments TO wp_', @newBlogID, '_comments;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+-- SET @renameTablesStatement = CONCAT('RENAME TABLE wp_links TO wp_', @newBlogID, '_links;');
+-- PREPARE renameTablesStatement FROM @renameTablesStatement;
+-- EXECUTE renameTablesStatement;
+-- DEALLOCATE PREPARE renameTablesStatement;
+
+-- SET @renameTablesStatement = CONCAT('RENAME TABLE wp_options TO wp_', @newBlogID, '_options;');
+-- PREPARE renameTablesStatement FROM @renameTablesStatement;
+-- EXECUTE renameTablesStatement;
+-- DEALLOCATE PREPARE renameTablesStatement;
+
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_postmeta TO wp_', @newBlogID, '_postmeta;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_posts TO wp_', @newBlogID, '_posts;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_term_relationships TO wp_', @newBlogID, '_term_relationships;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_term_taxonomy TO wp_', @newBlogID, '_term_taxonomy;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+SET @renameTablesStatement = CONCAT('RENAME TABLE wp_terms TO wp_', @newBlogID, '_terms;');
+PREPARE renameTablesStatement FROM @renameTablesStatement;
+EXECUTE renameTablesStatement;
+DEALLOCATE PREPARE renameTablesStatement;
+
+-- NOTE: We don't need to add these, since we're dealing with a SQL patch the tables must
+-- be created with `sql-utils/wpdb_default_tables.sql`, which includes those columns.
+-- Add two columns to the end of the wp_users table, spam and deleted. Both are TINYINT(2).
+-- ALTER TABLE wp_users ADD spam tinyint(2) default 0;
+-- ALTER TABLE wp_users ADD deleted tinyint(2) default 0;
+
+-- Update the user ID's
+UPDATE wp_users SET ID = ID + @newUserID;
+UPDATE wp_usermeta SET user_id = user_id + @newUserID;
+
+-- Update the user meta ID's
+UPDATE wp_usermeta SET umeta_id = umeta_id + @newUmetaID;
+UPDATE wp_usermeta SET meta_key = CONCAT('wp_', @newBlogID, '_user_level') WHERE meta_key = 'wp_user_level';
+UPDATE wp_usermeta SET meta_key = CONCAT('wp_', @newBlogID, '_capabilities') WHERE meta_key = 'wp_capabilities';
+
+-- Set the post author ID's
+SET @postAuthorStatement = CONCAT('UPDATE wp_', @newBlogID, '_posts SET post_author = post_author + ', @newUserID);
+PREPARE postAuthorStatement FROM @postAuthorStatement;
+EXECUTE postAuthorStatement;
+DEALLOCATE PREPARE postAuthorStatement;
+
+-- Usually you'll need to update links to uploaded files in the posts table,
+-- depends on your configuration and version of WP
+SET @postContentStatement = CONCAT(
+  'UPDATE wp_', @newBlogID, '_posts SET post_content = replace(post_content, \'wp-content/uploads/\', \'wp-content/blogs.dir/', @newBlogID, '/files/\')');
+PREPARE postContentStatement FROM @postContentStatement;
+EXECUTE postContentStatement;
+DEALLOCATE PREPARE postContentStatement;
+
+SET @commentAuthorStatement = CONCAT('UPDATE wp_', @newBlogID, '_comments SET user_id = user_id + ', @newUserID);
+PREPARE commentAuthorStatement FROM @commentAuthorStatement;
+EXECUTE commentAuthorStatement;
+DEALLOCATE PREPARE commentAuthorStatement;
+
+-- Find the users that are not authors, editors or admins
+-- and store their ID's in a temporary table. We'll delete
+-- these user records.
+drop temporary table if exists npq_delete_these_users;
+create temporary table if not exists npq_delete_these_users
+    select ID from wp_users join wp_usermeta on ID = user_id
+    where meta_key = CONCAT('wp_', @newBlogID, '_capabilities')
+    and not (
+        meta_value LIKE "%author%"
+        OR meta_value LIKE "%editor%"
+        OR meta_value LIKE "%administrator%"
+    );
+
+-- Delete user records where their ID exists in the
+-- to-be-delete temporary table created above.
+DELETE wp_users FROM wp_users JOIN npq_delete_these_users ON wp_users.ID = npq_delete_these_users.ID;
+
+-- Also delete any user meta associated with the users
+-- we want to scrub.
+DELETE wp_usermeta FROM wp_usermeta join npq_delete_these_users on user_id = ID;
+
+-- Go back through and delete user meta for records
+-- that have no match in the wp_users table.
+DELETE wp_usermeta FROM wp_usermeta
+    LEFT OUTER JOIN wp_users ON user_id = ID
+    WHERE ID IS NULL;
